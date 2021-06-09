4b4193a7364f95d88598ac1660876f5eff64ee0d
Magali Huertas <huertasmagali@gmail.com>
2021-06-08 21:29:51 -0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
+        <img src="/images/logo_unik.png" alt="Unik logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>Nosotros</li>
+                <li>Productos Gato</li>
+                <li>Productos Perro</li>
+            </ul>
+        </nav>
+    )
+}
